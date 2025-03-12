85c566baf2f5b39e425b5ef63dc10c225747703f
AlbeertoDZ <albertodiezalcobendas@gmail.com>
2025-03-12 20:33:46 +0100

    ejercicio_acabado_sin_correccion

--- scripts.js
+++ scripts.js
@@ -70,7 +70,7 @@ for (let i = 0; i < enlacesSinVisita.length; i++) {
       })
     })
 
-//4. 4. Crea una función de nombre `getRandom` que acepte un array con valores y devuelva uno de ellos de manera aleatoria.
+//4. Crea una función de nombre `getRandom` que acepte un array con valores y devuelva uno de ellos de manera aleatoria.
 
 const colors = ['red', 'blue', 'green'];
 
@@ -78,3 +78,60 @@ const getRandom = function(array) {
   return array[Math.floor(Math.random() * array.length)];
 }
 console.log(getRandom(colors));
+
+//5. Utiliza la función creada `getRandom` para utilizar colores aleatorios de una paleta que hayas escogido de [coolors.co](https://coolors.co/palettes/trending) en los apartados anteriores.
+
+  //5.2.1 Párrafos: Cambia el color del texto y el de fondo por uno cualquiera.
+  const paletaColores = ['#D4AFB9', '#D1CFE2', '#9CADCE', '#7EC4CF', '#52B2CF'];
+
+  document.querySelectorAll("p").forEach(function(parrafo){
+    parrafo.addEventListener("click", function(){
+      parrafo.style.color = getRandom(paletaColores);
+      parrafo.style.background = getRandom(paletaColores);
+    })
+  })
+
+  //5.2.2 Bloques de article o section: Cambia el color de fondo.
+
+  document.querySelectorAll("article").forEach(function (articulo) {
+    articulo.addEventListener("click", function () {
+      articulo.style.background=getRandom(paletaColores);
+    })
+  })
+
+
+  //5.3.1 Párrafos: Cambia el color del texto y el de fondo por uno cualquiera.
+  document.querySelectorAll("p").forEach(function(parrafo) {
+    parrafo.addEventListener("mouseover", function() {
+      parrafo.style.color=getRandom(paletaColores);
+      parrafo.style.background=getRandom(paletaColores);
+    })
+  });
+
+  //5.3.2 Bloques de `article` o `section`: Color de fondo distinto al de párrafo.
+
+  document.querySelectorAll("article").forEach(function(articulo){
+    articulo.addEventListener("mouseover", function() {
+      articulo.style.background=getRandom(paletaColores);
+    })
+  })
+
+//6. Utiliza la función creada `getRandom` para utilizar _gifs_ aleatorios  en los apartados anteriores.
+
+  const gifAleatorio = [
+    'assets/abracadabra.gif',
+    'assets/magic-1.gif',
+    'assets/magic-2.gif',
+    'assets/magic-3.gif',
+    'assets/magic-4.gif',
+    'assets/magic-5.gif',
+    'assets/magic-6.gif'
+  ]; 
+
+  //6.3.1 Imágenes: Cambia la imagen por el gif `abracadabra.gif`.
+  document.querySelectorAll("img").forEach(function(imagenes) {
+    imagenes.addEventListener("click", function () {
+      imagenes.setAttribute("src", getRandom(gifAleatorio));
+    })
+  })
+
